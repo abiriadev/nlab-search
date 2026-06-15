0af981e7031dad78b68dc28b5e7ce9ffba613911
Abiria <oro3673@gmail.com>
2026-06-15 05:19:51 -0400

    init

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = (
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          # The default `nodejs` in nixpkgs lags badly (often v14); pin a recent major.
+          packages = [
+            nodejs
+            pnpm
+          ];
+        };
+      }
+    )
+  );
+}
